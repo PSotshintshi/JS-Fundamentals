91e754cf8c38da4f797cb4e6ed06ef3c80fbdaa0
Phumeza_Sotshintshi <sotshintship@gmail.com>
2025-05-22 06:22:00 +0200

    This is a value argument

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,4 +1,10 @@
-let numPeople = 10
+/**let numPeople = 10
 if (numPeople >= 10){
   console.log("No argument");
-}else console.log("School");
+}else console.log("School");**/
+const numAvg = process.argv[2];
+if (numAvg !== undefined) {
+  console.log(numAvg);
+} else {
+  console.log("No argument");
+}
