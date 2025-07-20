b75b5787cbca84c50bbdcd2c5aff694f2f6450e5
Phumeza_Sotshintshi <sotshintship@gmail.com>
2025-07-20 20:36:51 +0200

    commit updates

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,6 +1,14 @@
-function Navbar(){
-    return(
-        <a href="./Home">Home</a>
-    )
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav style={{ padding: '1rem', backgroundColor: '#f0f0f0' }}>
+      <Link to="/" style={{ marginRight: '20px' }}>Home</Link>
+      <Link to="/about" style={{ marginRight: '20px' }}>About</Link>
+      <Link to="/services" style={{ marginRight: '20px' }}>Services</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+  );
 }
+
 export default Navbar;
