b552338b21ffa7c14282a449e87ee43335b20ff1
Phumeza_Sotshintshi <sotshintship@gmail.com>
2025-07-20 20:00:40 +0200

    more commits

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,21 +1,22 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-"react-router-dom", "Route", "path", "Routes", "element"
-import Home from '.Home';
-import About from './About';
-import Services from './Services';
-import Contact from './Contact';
-import Navbar from './Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
